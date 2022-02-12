56e443801bd0687133c3bcd32d8ae55785172d44
GomTron <76454082+GomTron@users.noreply.github.com>
2022-02-12 14:51:04 -0500

    Added .fit and .pred functionality
    Replaced old gradient descent method with .fit and added the .pred method to facilitate making predictions using trained model

--- LinearRegression.py
+++ LinearRegression.py
@@ -4,27 +4,36 @@ from tqdm import tqdm
 
 class LinearRegression:
 
-    @staticmethod
-    def gradient_descent(train_data, labels, learning_rate=0.05, epochs=1000):
+    def __init__(self):
+        self.parameters = None
+
+    def fit(self, X, y, learning_rate=0.05, epochs=1000):
         """
         The gradient descent algorithm. Returns the theta parameters that are iteratively modified
         to improve the accuracy of the hypothesis function.
 
-        :param train_data: numpy.ndarray
-        :param labels: numpy.ndarray
+        :param X: numpy.ndarray
+        :param y: numpy.ndarray
         :param learning_rate: int
         :param epochs: int
         :return: numpy.ndarray
         """
-        if len(train_data.shape) == 1:
+        if len(X.shape) == 1:
             num_features = 1
         else:
-            num_features = train_data.shape[1]
+            num_features = X.shape[1]
 
         theta = np.ones(shape=num_features + 1)
-        x_0 = np.ones_like(labels)
-        x = np.column_stack([x_0, train_data])
+        x_0 = np.ones_like(y)
+        x = np.column_stack([x_0, X])
         for _ in tqdm(range(epochs)):
-            cost = np.dot(x, theta) - labels
-            theta = theta - (learning_rate * (1 / len(labels)) * np.dot(x.T, cost))
-        return theta
+            cost = np.dot(x, theta) - y
+            theta = theta - (learning_rate * (1 / len(y)) * np.dot(x.T, cost))
+        self.parameters = theta
+
+    def pred(self, X):
+        y_pred = []
+        for i in range(len(X)):
+            pred = np.dot(self.parameters, np.insert(X[i], 0, 1))
+            y_pred.append(pred)
+        return y_pred
